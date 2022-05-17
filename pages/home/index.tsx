93eaeca27d826f07d24b734ce4dd7309f5c6a89a
iddqd <chris.walker.beng@gmail.com>
2022-05-17 17:01:01 +0100

    adding css modules to home

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,24 +2,25 @@ import React from "react";
 // import Map from "./Map";
 // import { Link } from "react-router-dom";
 import ContactUsComponent from "../../components/ContactUsComponent";
+import styles from "./home.module.css";
 
 function Home() {
   return (
     <>
-      <div className="hero-image"></div>
+      <div className={styles["hero-image"]}></div>
 
-      <div className="about-us-container">
-        <div className="about-header">About Us</div>
+      <div className={styles["about-us-container"]}>
+        <div className={styles["about-header"]}>About Us</div>
 
-        <div className="about-us-split-content">
-          <div className="about-us-split-text">
-            <div className="about-us-split-text-title">
+        <div className={styles["about-us-split-content"]}>
+          <div className={styles["about-us-split-text"]}>
+            <div className={styles["about-us-split-text-title"]}>
               Bright Eyes Animal Sanctuary
             </div>
 
-            <h1 className="about-us-split-text-description">
+            <h1 className={styles["about-us-split-text-description"]}>
               Bright Eyes was established in 1989 and is based in{" "}
-              <span className="slanted-about-us-bold-desc">
+              <span className={styles["slanted-about-us-bold-desc"]}>
                 Co.Fermanagh, Northern Ireland{" "}
               </span>
               .
@@ -27,7 +28,7 @@ function Home() {
               Our main purpose is to reduce unnecessary suffering and distress
               of companion animals through the provision of a rescue and
               re-homing service. We operate a strict{" "}
-              <span className="slanted-about-us-bold-desc">
+              <span className={styles["slanted-about-us-bold-desc"]}>
                 no kill policy
               </span>{" "}
               at Bright Eyes and if an animal, for any reason, cannot get a home
@@ -41,82 +42,85 @@ function Home() {
             </h1>
           </div>
 
-          <div className="about-us-split-image"> </div>
+          <div className={styles["about-us-split-image"]}> </div>
         </div>
 
-        {/* <Link to="/about"> */}
-        <button type="button" className="button about-us-read-more-button">
+        {/* <Link to="/about"]}> */}
+        <button
+          type="button"
+          className={styles["button about-us-read-more-button"]}
+        >
           Learn More
         </button>
         {/* </Link> */}
       </div>
 
-      <div className="stats-box-container">
-        <div className="stats-box-1">
-          <div className="stats-box-title">2000+</div>
-          <div className="stats-box-info">Pets Adopted</div>
+      <div className={styles["stats-box-container"]}>
+        <div className={styles["stats-box-1"]}>
+          <div className={styles["stats-box-title"]}>2000+</div>
+          <div className={styles["stats-box-info"]}>Pets Adopted</div>
         </div>
-        <div className="stats-box-2">
-          <div className="stats-box-title">20+</div>
-          <div className="stats-box-info">Volunteers</div>
+        <div className={styles["stats-box-2"]}>
+          <div className={styles["stats-box-title"]}>20+</div>
+          <div className={styles["stats-box-info"]}>Volunteers</div>
         </div>
-        <div className="stats-box-3">
-          <div className="stats-box-title">30+</div>
-          <div className="stats-box-info">Years Experience</div>
+        <div className={styles["stats-box-3"]}>
+          <div className={styles["stats-box-title"]}>30+</div>
+          <div className={styles["stats-box-info"]}>Years Experience</div>
         </div>
       </div>
 
-      <div className="get-involved-container-home">
-        <div className="get-involved-header">Get Involved</div>
+      <div className={styles["get-involved-container-home"]}>
+        <div className={styles["get-involved-header"]}>Get Involved</div>
 
-        <div className="get-involved-split-container">
-          <div className="get-involved-split">
-            <div className="involved-main-text"> Adopt</div>
-            <div className="involved-image-1"></div>
-            {/* <Link to="/adoption"> */}
+        <div className={styles["get-involved-split-container"]}>
+          <div className={styles["get-involved-split"]}>
+            <div className={styles["involved-main-text"]}> Adopt</div>
+            <div className={styles["involved-image-1"]}></div>
+            {/* <Link to="/adoption"]}> */}
             <button
               type="button"
               data-testid={"moreInfoAdoptButton"}
-              className="button get-involved-more-info-button"
+              className={styles["button get-involved-more-info-button"]}
             >
               More Info
             </button>
             {/* </Link> */}
-            <div className="involved-main-subtext">
+            <div className={styles["involved-main-subtext"]}>
               Take a look at our pets for Adoption. Join the 2000+ other people
               and find the perfect pet for your home!
             </div>
           </div>
-          <div className="get-involved-split">
-            <div className="involved-main-text"> Donate</div>
-            <div className="involved-image-2"> </div>
-            {/* <Link to="/donate"> */}
+          <div className={styles["get-involved-split"]}>
+            <div className={styles["involved-main-text"]}> Donate</div>
+            <div className={styles["involved-image-2"]}> </div>
+            {/* <Link to="/donate"]}> */}
             <button
               type="button"
               data-testid={"moreInfoDonateButton"}
-              className="button get-involved-more-info-button"
+              className={styles["button get-involved-more-info-button"]}
             >
               More Info
             </button>
             {/* </Link> */}
-            <div className="involved-main-subtext">
+            <div className={styles["involved-main-subtext"]}>
               We receive no government funding and rely purely on the generosity
               of the public to help us continue our work.
             </div>
           </div>
-          <div className="get-involved-split">
-            <div className="involved-main-text"> Volunteer</div>
-            <div className="involved-image-3"> </div>
-            {/* <Link to="/forms/volunteer"> */}
+          <div className={styles["get-involved-split"]}>
+            <div className={styles["involved-main-text"]}> Volunteer</div>
+            <div className={styles["involved-image-3"]}> </div>
+            {/* <Link to="/forms/volunteer"]}> */}
             <button
               type="button"
               data-testid={"moreInfoVolunteerButton"}
-              className="button get-involved-more-info-button"
+              className={styles["button get-involved-more-info-button"]}
             >
               More Info
             </button>
             {/* </Link> */}
-            <div className="involved-main-subtext">
+            <div className={styles["involved-main-subtext"]}>
               Our fantastic volunteers are the backbone of Bright Eyes. Want to
               help care for the animals and earn some valuable experience?
             </div>
@@ -125,11 +129,11 @@ function Home() {
       </div>
 
       <ContactUsComponent />
-      <div className="info-container">
-        <div className="info-split-container">
-          <div className="info-split">
-            <div className="info-header">Location</div>
-            <div className="info-main-text">
+      <div className={styles["info-container"]}>
+        <div className={styles["info-split-container"]}>
+          <div className={styles["info-split"]}>
+            <div className={styles["info-header"]}>Location</div>
+            <div className={styles["info-main-text"]}>
               53 Killymittan Road,
               <br />
               BT94 2FW,
@@ -137,27 +141,28 @@ function Home() {
               Ballinamallard
             </div>
           </div>
-          <div className="info-split">
-            <div className="info-header">Hours</div>
-            <div className="info-main-text">
+          <div className={styles["info-split"]}>
+            <div className={styles["info-header"]}>Hours</div>
+            <div className={styles["info-main-text"]}>
               {" "}
-              <span className="bold-noto">Mon - Sun:</span> 12:00 – 15:00
+              <span className={styles["bold-noto"]}>Mon - Sun:</span> 12:00 –
+              15:00
               <br />
             </div>
           </div>
-          <div className="info-split">
-            <div className="info-header">Contact Info</div>
-            <div className="info-main-text">
-              <span className="bold-noto">Mobile:</span> 07710607816
+          <div className={styles["info-split"]}>
+            <div className={styles["info-header"]}>Contact Info</div>
+            <div className={styles["info-main-text"]}>
+              <span className={styles["bold-noto"]}>Mobile:</span> 07710607816
               <br />
-              <span className="bold-noto">Phone:</span> 028 66 388885
+              <span className={styles["bold-noto"]}>Phone:</span> 028 66 388885
               <br />
-              <span className="bold-noto">Email:</span>{" "}
+              <span className={styles["bold-noto"]}>Email:</span>{" "}
               brighteyes.sanctuary@btinternet.com
             </div>
           </div>
-          <div className="info-split">
-            <div className="info-header">Follow Us</div>
+          <div className={styles["info-split"]}>
+            <div className={styles["info-header"]}>Follow Us</div>
             <a
               target="_blank"
               rel="noreferrer"
