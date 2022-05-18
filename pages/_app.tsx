a21d413b2bc510e596f805f6f2b68d9d0c37dccc
iddqd <chris.walker.beng@gmail.com>
2022-05-18 14:34:51 +0100

    Added css, images and navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import NavbarComponent from "../components/NavbarComponent";
+import NavbarComponent from "../components/Navbar/NavbarComponent";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
