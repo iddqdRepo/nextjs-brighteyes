604207542ec5ae324a636ba44789f43e71883dff
iddqd <chris.walker.beng@gmail.com>
2022-05-18 12:53:40 +0100

    Moved images to public folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import NavbarComponent from "../components/NavbarComponent";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavbarComponent />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
