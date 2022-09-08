93a99995e71e8be24b77ac4ba12d3836e974022c
iddqdRepo <chris.walker.beng@gmail.com>
2022-09-08 16:31:06 +0100

    Tailwind Home Transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,135 @@
+/* eslint-disable @next/next/no-img-element */
+
 import React from "react";
 import Map from "../components/MapComponent/Map.js";
+// import Image from "next/image.js";
 // import Link from "next/link";
 import ContactUsComponent from "../components/ContactUs/ContactUsComponent";
-// import styles from "./home.module.css";
+import { RoughNotation } from "react-rough-notation";
+import { Icon } from "@iconify/react";
 
 function Home() {
   return (
     <>
-      <h1 className="text-red-300">Hello world!</h1>
-      <div className="hero-image"></div>
+      {/* <img  className="hero-image"></div> */}
+      <div className="relative w-full h-full bg-center bg-no-repeat bg-contain">
+        <img src="/DogHero4.png" alt="" />
+        <div className="absolute w-full rotate-180 -bottom-1 top-1/3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 283.5 19.6"
+            preserveAspectRatio="none"
+          >
+            <path
+              className="fill-white opacity-20"
+              d="M0 0v19L142 4l142 15V0z"
+            />
+            <path
+              className="fill-white opacity-20"
+              d="M0 0v13l142-9 142 9V0z"
+            />
+            <path className="fill-white opacity-20" d="M0 0v6l142-2 142 2V0z" />
+            <path className="fill-white" d="M0 0v1l142 3 142-3V0z" />
+          </svg>
+        </div>
+        <div className="absolute flex top-1/3 left-20">
+          <span className="text-5xl font-semibold font-poppins">
+            Don&apos;t buy, &nbsp;
+            <RoughNotation
+              iterations={5}
+              type="circle"
+              show={true}
+              color="#8b3479"
+              strokeWidth={1}
+            >
+              adopt!
+            </RoughNotation>
+          </span>
+        </div>
+      </div>
+      <div className="flex flex-col items-center ">
+        <div className="flex w-5/6 mt-16 mb-10 text-xl font-semibold font-poppins">
+          <div className="flex items-center">
+            <div className="mr-2 border-b-4 border-[#8b3479] w-7"></div>
+          </div>
+          About Us
+        </div>
+      </div>
 
-      <div className="about-us-container">
-        <div className="about-header">About Us</div>
+      <div className="flex flex-col items-center">
+        {/* <div className="about-us-container"> */}
+        {/* <div className="about-header">About Us</div> */}
 
-        <div className="about-us-split-content">
-          <div className="about-us-split-text">
-            <div className="about-us-split-text-title">
+        {/* <div className="about-us-split-content"> */}
+        <div className="flex flex-col w-5/6 xl:flex-row h-2/4">
+          {/* <div className="about-us-split-text"> */}
+          <div className=" basis-2/3">
+            {/* <div className="about-us-split-text-title"> */}
+            <div className=" pb-7 pt-3  text-[#8b3479] font-normal font-poppins text-4xl">
               Bright Eyes Animal Sanctuary
             </div>
 
-            <h1 className="about-us-split-text-description">
+            {/* <h1 className="about-us-split-text-description"> */}
+            <h1 className="w-11/12 text-lg font-roboto">
               Bright Eyes was established in 1989 and is based in{" "}
-              <span className="bolding">Co.Fermanagh, Northern Ireland </span>
+              <span className="font-semibold ">
+                Co.Fermanagh, Northern Ireland
+              </span>
               .
               <br />
               Our main purpose is to reduce unnecessary suffering and distress
               of companion animals through the provision of a rescue and
-              re-homing service. We operate a strict{" "}
-              <span className="bolding">no kill policy</span> at Bright Eyes and
-              if an animal, for any reason, cannot get a home it will have
-              shelter here with us for life. <br />
-              We support the local community and offer volunteering
-              opportunities, training and education on animal welfare. <br />
-              We receive no government funding and rely purely on the generosity
-              of the public to help us continue our work.
-              <br /> All animals rehomed from us are neutered/spayed,
-              vaccinated, microchipped, dewormed and deflead.
+              re-homing service. We operate a &nbsp;
+              <RoughNotation
+                iterations={5}
+                type="highlight"
+                show={true}
+                color="#8b3479"
+                strokeWidth={3}
+                animate={false}
+              >
+                <span className="text-white">strict no kill policy</span>
+              </RoughNotation>
+              {/* <span className="font-semibold"></span> */}&nbsp; at Bright
+              Eyes and if an animal, for any reason, cannot get a home it will
+              have shelter here with us for life. <br />
             </h1>
+            <div className="flex flex-col w-11/12 mt-10 text-lg font-roboto ">
+              <div className="flex mb-5">
+                <Icon icon="mdi:bone" color="#8b3479" width="30" height="30" />
+                &nbsp;
+                <span>
+                  We support the local community and offer volunteering
+                  opportunities, training and education on animal welfare
+                </span>
+              </div>
+              <div className="flex mb-5">
+                <Icon icon="mdi:bone" color="#8b3479" width="30" height="30" />
+                &nbsp;
+                <span>
+                  We receive no government funding and rely purely on the
+                  generosity of the public to help us continue our work.
+                </span>
+              </div>
+              <div className="flex mb-5">
+                <Icon icon="mdi:bone" color="#8b3479" width="30" height="30" />
+                &nbsp;
+                <span>
+                  All animals rehomed from us are neutered/spayed, vaccinated,
+                  microchipped, dewormed and deflead.
+                </span>
+              </div>
+            </div>
           </div>
 
-          <div className="about-us-split-image"> </div>
+          {/* <div className="about-us-split-image"> </div> */}
+          <div className="flex justify-center basis-1/3">
+            <img
+              className="w-2/5 bg-center bg-no-repeat xl:w-auto xl:h-4/5 rounded-xl"
+              src="/puppyDogEyes.jpg"
+              alt=""
+            />
+          </div>
         </div>
         {/* <Link href={`/about`}>
           <button
@@ -53,7 +143,6 @@ function Home() {
           </button>
         </Link> */}
       </div>
-
       <div className="stats-box-container">
         <div className="stats-box-1">
           <div className="stats-box-title">2000+</div>
@@ -68,7 +157,6 @@ function Home() {
           <div className="stats-box-info">Years Experience</div>
         </div>
       </div>
-
       <div className="get-involved-container-home">
         <div className="get-involved-header">Get Involved</div>
 
@@ -135,7 +223,6 @@ function Home() {
           </div>
         </div>
       </div>
-
       <ContactUsComponent />
       <div className="info-container">
         <div className="info-split-container">
@@ -197,7 +284,6 @@ function Home() {
           </div>
         </div>
       </div>
-
       <Map />
     </>
   );
