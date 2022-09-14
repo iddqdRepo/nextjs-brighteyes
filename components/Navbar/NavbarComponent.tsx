f98c64ffed02ae23245c4d7150790c78d8c9d905
iddqdRepo <chris.walker.beng@gmail.com>
2022-09-14 12:39:44 +0100

    Added Mobile Nav

--- components/Navbar/NavbarComponent.tsx
+++ components/Navbar/NavbarComponent.tsx
@@ -7,7 +7,14 @@ import { useRouter } from "next/router";
 
 function NavbarComponent() {
   const router = useRouter();
+  const mobileNavDropdownRef = useRef<null | HTMLDivElement>(null);
+  const defaultNavRef = useRef<null | HTMLUListElement>(null);
 
+  const hideMobileNav = () => {
+    if (mobileNavDropdownRef.current !== null) {
+      mobileNavDropdownRef.current.classList.toggle("hidden");
+    }
+  };
   interface NavRefInterface {
     home: HTMLDivElement | null;
     about: HTMLDivElement | null;
@@ -15,6 +22,27 @@ function NavbarComponent() {
     donate: HTMLDivElement | null;
     forms: HTMLDivElement | null;
   }
+
+  const MobileNavListItem = ({
+    text,
+    path,
+  }: {
+    text: string;
+    path: string;
+  }) => {
+    return (
+      <li
+        className="flex py-4 mx-4 text-lg font-medium text-black font-poppins justify-left"
+        onClick={hideMobileNav}
+      >
+        <Link href={path ? `/${path}` : `/`}>
+          <a className="text-lg font-normal leading-6 text-black" href="#">
+            {text}
+          </a>
+        </Link>
+      </li>
+    );
+  };
 
   const navRefs = useRef<NavRefInterface>({
     home: null,
@@ -45,43 +73,18 @@ function NavbarComponent() {
     }
   };
 
-  // const ListItem = () => {
-  //   return (
-  //     <li onClick={() => handleNavPaw(navRefs.current.home)}>
-  //       <Link href="/">
-  //         <a className="flex items-center mr-5 text-lg font-medium font-poppins">
-  //           <div className="w-5">
-  //             <div
-  //               ref={(e) =>
-  //                 navRefs.current.home === null
-  //                   ? (navRefs.current.home = e)
-  //                   : console.log("NotAdded")
-  //               }
-  //               className="hidden"
-  //             >
-  //               <Icon
-  //                 icon="foundation:paw"
-  //                 color="#8b3479"
-  //                 width="20"
-  //                 height="20"
-  //               />
-  //             </div>
-  //           </div>
-  //           <span className="cursor-pointer">Home</span>
-  //         </a>
-  //       </Link>
-  //     </li>
-  //   );
-  // };
-
   return (
     <>
       <nav className="flex items-center justify-between w-full h-20">
-        <div className="flex flex-col items-end justify-end w-auto h-16">
-          <img className="w-auto h-full" src="/logo-nav.png" alt="" />
+        <div className="flex flex-col justify-end">
+          <img
+            className="object-cover w-full h-16 "
+            src="/logo-nav.png"
+            alt=""
+          />
         </div>
-        <div className="flex">
-          <ul className="flex justify-center ">
+        <div className="hidden lg:flex">
+          <ul className="flex flex-wrap justify-center">
             <li onClick={() => handleNavPaw(navRefs.current.home)}>
               <Link href="/">
                 <a className="flex items-center mr-5 text-lg font-medium font-poppins">
@@ -187,8 +190,8 @@ function NavbarComponent() {
             </li>
           </ul>
         </div>
-        <div className="flex justify-between w-3/12 pr-5">
-          <div className="flex items-center justify-center">
+        <div className="justify-between hidden pr-5 lg:flex">
+          <div className="flex items-center justify-center w-48 lg">
             <div className="flex items-center justify-center w-10 h-10 mr-3 border rounded-lg shadow-xl">
               <Icon
                 icon="akar-icons:location"
@@ -197,11 +200,11 @@ function NavbarComponent() {
                 height="25"
               />
             </div>
-            <span className="flex text-sm font-medium font-poppins">
+            <span className="flex text-xs font-medium font-poppins">
               53 Killymittan Road, <br /> BT94 2FW, Ballinamallard
             </span>
           </div>
-          <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center w-44">
             <div className="flex items-center justify-center w-10 h-10 mr-3 border rounded-lg shadow-xl">
               <Icon
                 icon="carbon:phone-voice"
@@ -215,176 +218,30 @@ function NavbarComponent() {
             </span>
           </div>
         </div>
+
+        <div onClick={hideMobileNav} className="flex mr-10 lg:hidden">
+          <button>
+            <Icon icon="fontisto:nav-icon" color="black" />
+          </button>
+        </div>
       </nav>
+
+      <div className="relative z-50 flex w-full">
+        <div
+          ref={mobileNavDropdownRef}
+          className="absolute flex-col hidden w-full bg-white lg:hidden"
+        >
+          <ul ref={defaultNavRef} className="">
+            <MobileNavListItem text={"Home"} path={""} />
+            <MobileNavListItem text={"About"} path={"about"} />
+            <MobileNavListItem text={"Adoption"} path={"adoption"} />
+            <MobileNavListItem text={"Donate"} path={"donate"} />
+            <MobileNavListItem text={"Forms"} path={"forms"} />
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
-
-// function NavbarComponent() {
-//   interface NavLinks {
-//     [key: string]: string;
-//   }
-
-//   const navLinks: NavLinks[] = [
-//     { Home: "/" },
-//     { "About Us": "/about" },
-//     { Adoption: "/adoption" },
-//     { Donate: "/donate" },
-//     { Volunteer: "/forms/volunteer" },
-//     { Forms: "/forms" },
-//   ];
-
-//   const refs = useRef<HTMLAnchorElement[]>([]);
-
-//   const hideNavRef = useRef<any>();
-
-//   const activeToggle = (ref: HTMLAnchorElement, text: string) => {
-//     //^ loop through each nav item, if it's the clicked link, set the classname to active-link
-//     //^ remove active-link from all others so they don't stay highlighted
-//     if (refs.current) {
-//       refs.current.forEach((element: HTMLAnchorElement) => {
-//         if (element.innerText !== text) {
-//           element.className = `${styles.navitem}`;
-//         } else {
-//           element.className = `${[styles.navitem, styles.activelink].join(
-//             " "
-//           )}`;
-//         }
-//       });
-//     }
-//   };
-
-//   const hideMobileNavOnLinkClick = (
-//     current: { checked: boolean } | undefined
-//   ) => {
-//     if (current) {
-//       current.checked = false;
-//     }
-//   };
-
-//   return (
-//     <>
-//       <nav className={styles["mobile-nav"]}>
-//         <div id="menuToggle">
-//           <input ref={hideNavRef} type="checkbox" />
-//           <span></span>
-//           <span></span>
-//           <span></span>
-//           <ul id="menu">
-//             <Link href="/">
-//               <li
-//                 onClick={() => {
-//                   hideMobileNavOnLinkClick(hideNavRef.current);
-//                 }}
-//               >
-//                 Home
-//               </li>
-//             </Link>
-//             <Link href="/about">
-//               <li
-//                 onClick={() => {
-//                   hideMobileNavOnLinkClick(hideNavRef.current);
-//                 }}
-//               >
-//                 About Us
-//               </li>
-//             </Link>
-//             <Link href="/adoption">
-//               <li
-//                 onClick={() => {
-//                   hideMobileNavOnLinkClick(hideNavRef.current);
-//                 }}
-//               >
-//                 Adoption
-//               </li>
-//             </Link>
-//             <Link href="/donate">
-//               <li
-//                 onClick={() => {
-//                   hideMobileNavOnLinkClick(hideNavRef.current);
-//                 }}
-//               >
-//                 Donate
-//               </li>
-//             </Link>
-//             <Link href="/forms/volunteer">
-//               <li
-//                 onClick={() => {
-//                   hideMobileNavOnLinkClick(hideNavRef.current);
-//                 }}
-//               >
-//                 Volunteer
-//               </li>
-//             </Link>
-//             <Link href="/forms">
-//               <li
-//                 onClick={() => {
-//                   hideMobileNavOnLinkClick(hideNavRef.current);
-//                 }}
-//               >
-//                 Forms
-//               </li>
-//             </Link>
-//           </ul>
-//         </div>
-//       </nav>
-//       <nav className={styles["nav"]}>
-//         <div className={styles["nav-logo"]}></div>
-//         <div className={styles["social-links-container"]}>
-//           <div className={styles["social"]}>
-//             <a
-//               target="_blank"
-//               rel="noreferrer"
-//               href="https://www.facebook.com/brighteyes.a.s/"
-//             >
-//               <span
-//                 className={styles.iconify}
-//                 data-icon="akar-icons:facebook-fill"
-//                 data-width="20"
-//                 data-height="20"
-//               ></span>
-//             </a>
-//             <a
-//               target="_blank"
-//               rel="noreferrer"
-//               href="https://www.instagram.com/brighteyesanimalsanctuary"
-//             >
-//               <span
-//                 className={styles.iconify}
-//                 data-icon="akar-icons:instagram-fill"
-//                 data-width="20"
-//                 data-height="20"
-//               ></span>
-//             </a>
-//           </div>
-//           <ul className={styles["nav__list"]}>
-//             {navLinks.map((el, index) => {
-//               //^ map thought and create nav links from the navLinks object
-//               //^ Create a specific useRef for each element in the map and store it in refs
-//               return (
-//                 <li key={Object.keys(el)[0]}>
-//                   <Link href={el[Object.keys(el)[0]]}>
-//                     <a
-//                       className={styles.navitem}
-//                       data-testid={`nav${Object.keys(el)[0]}`}
-//                       ref={(element) => {
-//                         refs.current[index] = element as HTMLAnchorElement;
-//                       }}
-//                       onClick={() => {
-//                         activeToggle(refs.current[index], Object.keys(el)[0]);
-//                       }}
-//                     >
-//                       {Object.keys(el)[0]}
-//                     </a>
-//                   </Link>
-//                 </li>
-//               );
-//             })}
-//           </ul>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// }
 
 export default NavbarComponent;
