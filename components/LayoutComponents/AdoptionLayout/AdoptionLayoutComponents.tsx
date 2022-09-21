c697e97dc77b00cfbbd28a769af702f7381ea657
iddqdRepo <chris.walker.beng@gmail.com>
2022-09-21 12:30:50 +0100

    Added Adoption page + refactor

---
+++ components/LayoutComponents/AdoptionLayout/AdoptionLayoutComponents.tsx
@@ -0,0 +1,100 @@
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+import React from "react";
+
+export const AdoptionCard = ({
+  name,
+  type,
+  age,
+  sex,
+  image,
+}: {
+  name: string;
+  type: string;
+  age: string;
+  sex: string;
+  image?: string;
+}) => {
+  return (
+    <>
+      <div className="flex flex-col mb-10 ml-3 mr-3">
+        {/* <img className="" src={image} alt="" /> */}
+        <div
+          className="w-64 bg-no-repeat bg-cover rounded-xl h-80"
+          style={{
+            backgroundImage: `url("${image}")`,
+          }}
+        ></div>
+        <Link href={"/"}>
+          <a className="flex justify-center w-full -mt-14">
+            <div className="flex flex-col w-11/12 bg-white border border-gray-300 rounded-md">
+              <span className="pt-3 pb-2 pl-6 pr-6 text-xl font-semibold font-poppins">
+                {name}
+              </span>
+              <span className="pb-2 pl-6 pr-6 text-[#8b3479] text-sm font-normal font-roboto">
+                {type}
+              </span>
+              <div className="flex justify-center w-full">
+                <span className="w-10/12 mb-2 border-b border-gray-100"></span>
+              </div>
+              <div className="flex justify-center text-sm font-normal font-roboto">
+                <div className="flex items-center mr-4">
+                  <Icon
+                    className="mr-2"
+                    icon={"akar-icons:cake"}
+                    inline={true}
+                    color="#8b3479"
+                  />
+                  <span>Age:&nbsp;</span>
+                  <span>{age} yrs</span>
+                </div>
+                <div className="flex items-center">
+                  <Icon
+                    className="mr-2"
+                    icon={"bi:gender-ambiguous"}
+                    inline={true}
+                    color="#8b3479"
+                  />
+                  <span>Sex:&nbsp;</span>
+                  <span>{sex}</span>
+                </div>
+              </div>
+            </div>
+          </a>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export const AdoptionIconContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="flex flex-col flex-wrap pt-10 sm:flex-nowrap lg:items-start w-fit">
+      {children}
+    </div>
+  );
+};
+
+export const IconText = ({ text }: { text: string }) => {
+  return (
+    <>
+      <div className="flex flex-col flex-wrap items-center justify-center pb-8 pl-2 pr-2 lg:flex-row">
+        <div className="flex rounded-full bg-[#8b3479] h-20 w-20 items-center justify-center">
+          <Icon
+            color="#ffffff"
+            icon="charm:circle-tick"
+            width="40"
+            height="40"
+          />
+        </div>
+        <span className="font-normal sm:pl-6 text-md font-poppins xl:text-2xl">
+          {text}
+        </span>
+      </div>
+    </>
+  );
+};
--- pages/adoption/index.tsx
+++ pages/adoption/index.tsx
@@ -1,264 +1,31 @@
-import React, { useState, useEffect } from "react";
-import styles from "./adoption.module.css";
-import Link from "next/link";
-import { image } from "../../public/testImage";
-import { Icon } from "@iconify/react";
-// import * as api from "../api/apiIndex";
-// import DonateSlantedComponent from "../../components/";
+import React from "react";
+import { FooterSection } from "../../components/common/CommonComponents";
+import {
+  AdoptionCardSection,
+  AdoptionCriteriaSection,
+} from "../../components/layoutComponents/AdoptionLayout/AdoptionLayout";
 
 function Adoption() {
-  interface animalInterface {
-    _id: string;
-    type: string;
-    name: string;
-    age: string;
-    yearsOrMonths: string;
-    breed: string;
-    size: string;
-    image: string;
-    suitableForChildren: string;
-    suitableForAnimals: string;
-    adopted: string;
-    desc: string;
-  }
-
-  // let navigate = useNavigate();
-  const [animals, setAnimals] = useState<animalInterface[] | null>(null);
-  const [filteredAnimals, setFilteredAnimals] = useState("");
-
-  useEffect(() => {
-    // const getAllAnimals = async () => {
-    //   const data = await api.fetchPets();
-    //   // console.log(data);
-    //   setAnimals(
-    //     data.data.filter((animal) => {`
-    //       return animal.adopted === "No";
-    //     })
-    //   );
-    //   setFilteredAnimals((animal) => {
-    //     return animal.adopted === "No";
-    //   });
-    // };
-    // getAllAnimals();
-    setAnimals([
-      {
-        _id: "string",
-        type: "string",
-        name: "string",
-        age: "string",
-        yearsOrMonths: "string",
-        breed: "string",
-        size: "string",
-        image: image,
-        suitableForChildren: "string",
-        suitableForAnimals: "string",
-        adopted: "string",
-        desc: "string",
-      },
-      {
-        _id: "number",
-        type: "number",
-        name: "number",
-        age: "number",
-        yearsOrMonths: "number",
-        breed: "number",
-        size: "number",
-        image: image,
-        suitableForChildren: "number",
-        suitableForAnimals: "number",
-        adopted: "number",
-        desc: "number",
-      },
-    ]);
-  }, []);
+  // interface animalInterface {
+  //   _id: string;
+  //   type: string;
+  //   name: string;
+  //   age: string;
+  //   yearsOrMonths: string;
+  //   breed: string;
+  //   size: string;
+  //   image: string;
+  //   suitableForChildren: string;
+  //   suitableForAnimals: string;
+  //   adopted: string;
+  //   desc: string;
+  // }
 
   return (
     <>
-      <div className={styles["adoption-container"]}>
-        <div className={styles["adoption-header"]}>Adoption Criteria</div>
-        <div className={styles["adoption-subtext"]}>
-          There are certain criteria that need to be met in order to adopt a pet
-          from Bright Eyes, please make sure you meet the criteria before
-          submitting your application.
-        </div>
-
-        <div className={styles["adoption-split-content"]}>
-          <div className={styles["adoption-split-points-left-container"]}>
-            <div className={styles["adoption-point-container"]}>
-              <Icon
-                className={styles["iconify-inline"]}
-                icon="bi:check-circle"
-              />
-              <span className={styles["adoption-point-text"]}>
-                {" "}
-                Enclosed Garden{" "}
-              </span>
-            </div>
-            <div className={styles["adoption-point-container"]}>
-              <Icon
-                className={styles["iconify-inline"]}
-                icon="bi:check-circle"
-              />
-              <span className={styles["adoption-point-text"]}>
-                {" "}
-                Sleeping Indoors{" "}
-              </span>
-            </div>
-            <div className={styles["adoption-point-container"]}>
-              <Icon
-                className={styles["iconify-inline"]}
-                icon="bi:check-circle"
-              />
-              <span className={styles["adoption-point-text"]}>
-                {" "}
-                Landlord Permission{" "}
-              </span>
-            </div>
-          </div>
-
-          <div className={styles["adoption-split-points-right-container"]}>
-            <div className={styles["adoption-point-container"]}>
-              <Icon
-                className={styles["iconify-inline"]}
-                icon="bi:check-circle"
-              />
-              <span className={styles["adoption-point-text"]}>
-                {" "}
-                Children 12+{" "}
-              </span>
-            </div>
-            <div className={styles["adoption-point-container"]}>
-              <Icon
-                className={styles["iconify-inline"]}
-                icon="bi:check-circle"
-              />
-              <span className={styles["adoption-point-text"]}>
-                {" "}
-                Compulsory Home Check{" "}
-              </span>
-            </div>
-            <div className={styles["adoption-point-container"]}>
-              <Icon
-                className={styles["iconify-inline"]}
-                icon="bi:check-circle"
-              />
-              <span className={styles["adoption-point-text"]}>
-                {" "}
-                Form Required{" "}
-              </span>
-            </div>
-          </div>
-        </div>
-        <Link href="/forms">
-          <button
-            type="button"
-            className={[styles["adoption-form-button"], styles.button].join(
-              " "
-            )}
-          >
-            Adoption Form
-          </button>
-        </Link>
-
-        <div className={styles["slanted-div-filter"]}>
-          <div className={styles["slanted-div-filter-content-container"]}>
-            <div className={styles["slanted-div-left-filter-text-container"]}>
-              <span className={styles["filter-header"]}>Filter</span> <br />
-              <br />
-              <div className={styles["filter-dropdown"]}>
-                <div className={styles["dropdown"]}>
-                  <select
-                    name="one"
-                    className={styles["dropdown-select"]}
-                    onChange={(e) => {
-                      setFilteredAnimals(e.target.value);
-                    }}
-                  >
-                    <option value="all">All</option>
-                    <option value="Dog">Dogs</option>
-                    <option value="Cat">Cats</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles["pet-viewer-content-container"]}>
-          {!animals ? (
-            <>
-              <div className={styles["Loading-ring"]}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-            </>
-          ) : (
-            animals
-              .filter((val) => {
-                if (filteredAnimals === "Dog") {
-                  return val.type === "Dog";
-                } else if (filteredAnimals === "Cat") {
-                  return val.type === "Cat";
-                } else {
-                  return val;
-                }
-              })
-              .map((key) => {
-                return (
-                  <Link
-                    key={key.name + key.age}
-                    href={`/adoption/viewBio?id=${key._id}`}
-                  >
-                    <div
-                      data-testid="AnimalContainer"
-                      className={styles["pet-container"]}
-                    >
-                      <div
-                        className={styles["pet-image"]}
-                        style={{
-                          backgroundImage: `url("${key.image}")`,
-                        }}
-                      ></div>
-                      <div className={styles["pet-age-weight"]}>
-                        <div className={styles["pet-weight"]}>
-                          <span
-                            className={styles["iconify-inline"]}
-                            data-icon="ion:scale-sharp"
-                          ></span>
-                          &nbsp;{key.size}
-                        </div>
-                        <div className={styles["pet-age"]}>
-                          <span
-                            className={styles["iconify-inline"]}
-                            data-icon="bi:calendar-week-fill"
-                          ></span>
-                          &nbsp;{key.age} {key.yearsOrMonths}
-                        </div>
-                      </div>
-                      {/* <div className={styles["pet-description"]}>{key.desc}</div> */}
-                      <div className={styles["pet-name"]}>{key.name}</div>
-
-                      {/* <Link href={`/adoption/viewBio?id=${key._id}`}> */}
-                      <button
-                        type="button"
-                        className={[
-                          styles["pet-read-more-link"],
-                          styles.button,
-                        ].join(" ")}
-                      >
-                        view {key.name}
-                      </button>
-                    </div>
-                  </Link>
-                );
-              })
-          )}
-        </div>
-      </div>
-
-      {/* <DonateSlantedComponent /> */}
+      <AdoptionCriteriaSection />
+      <AdoptionCardSection />
+      <FooterSection />
     </>
   );
 }
