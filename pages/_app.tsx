3fa7c4369773ef351feda0ac47fe0b04daf462be
iddqdRepo <chris.walker.beng@gmail.com>
2022-09-26 14:15:56 +0100

    Added log to debug build action

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import NavbarComponent from "../components/navbar/NavbarComponent";
+import NavbarComponent from "../components/Navbar/NavbarComponent";
 //test comment
 function MyApp({ Component, pageProps }: AppProps) {
   return (
