a5fa64797fd9b143ace0c42d3f8db502592fd04b
iddqdRepo <chris.walker.beng@gmail.com>
2022-10-28 10:16:06 +0100

    Added react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
 //test comment
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </>
   );
 }
