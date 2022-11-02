d2eef66fbe89e74321781bb0386143816dbbabc9
iddqdRepo <chris.walker.beng@gmail.com>
2022-11-02 13:36:19 +0000

    Added api routes

--- routes/formRoutes.tsx
+++ routes/formRoutes.tsx
@@ -1,4 +1,9 @@
 import axios from "axios";
+import {
+  GiftaidFormInterface,
+  PetAdoptionFormInterface,
+  VolunteerFormInterface,
+} from "../interfaces/interfaces";
 
 export const getPetForms = async () => {
   const petforms = await axios.get("http://localhost:3000/api/forms?type=pet");
@@ -25,4 +30,21 @@ export const deleteGiftAidForm = async (id: string) => {
 };
 export const deleteVolunteerForm = async (id: string) => {
   await axios.delete(`http://localhost:3000/api/forms/${id}?type=volunteer`);
+};
+
+export const udpatePetForm = async (data: PetAdoptionFormInterface) => {
+  await axios.put(`http://localhost:3000/api/forms/${data._id}?type=pet`, data);
+};
+
+export const udpateGiftAidForm = async (data: GiftaidFormInterface) => {
+  await axios.put(
+    `http://localhost:3000/api/forms/${data._id}?type=giftaid`,
+    data
+  );
+};
+export const udpateVolunteerForm = async (data: VolunteerFormInterface) => {
+  await axios.put(
+    `http://localhost:3000/api/forms/${data._id}?type=volunteer`,
+    data
+  );
 };
---
+++ routes/petRoutes.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { PetInterface } from "../interfaces/interfaces";
+
+export const getPets = async () => {
+  const pets = await axios.get("http://localhost:3000/api/pets");
+  return pets.data;
+};
+
+export const deletePet = async (id: string) => {
+  await axios.delete(`http://localhost:3000/api/pets/${id}`);
+};
+
+export const updatePet = async (data: PetInterface) => {
+  await axios.put(`http://localhost:3000/api/pets/${data._id}`, data);
+};
