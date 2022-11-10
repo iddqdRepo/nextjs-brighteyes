ff1562e9621419a918b0f9d4d55c142de87a2d1f
iddqdRepo <chris.walker.beng@gmail.com>
2022-11-10 16:13:03 +0000

    Added logout button to sidebar

--- adminComponents/AdminSidebarComponent.tsx
+++ adminComponents/AdminSidebarComponent.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+import React from "react";
 import { useState, useEffect } from "react";
 import { Icon } from "@iconify/react";
 import Link from "next/link";
+import axios from "axios";
+import router from "next/router";
 
 function AdminSidebarComponent(props: any) {
   const [toggleSelected, setToggleSelected] = useState();
@@ -50,6 +53,35 @@ function AdminSidebarComponent(props: any) {
             </span>
           </div>
         </Link>
+      </div>
+    );
+  };
+  const handleLogout = async (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    console.log("AWAITING LOGOUT, handlelogout");
+    const user = await axios.post("/api/auth/logout");
+    if (user.data.message) {
+      router.push("/admin");
+    }
+  };
+  const LogoutButton = () => {
+    return (
+      <div
+        className="flex flex-col items-center justify-center w-4/5 py-2 mb-4 text-sm rounded-sm font-poppins hover:cursor-pointer"
+        onClick={(e) => handleLogout(e)}
+      >
+        <div className="flex flex-row items-center justify-center w-3/5 md:justify-start">
+          <Icon
+            className="w-auto h-6 text-[#B0B0B8] group-hover:text-white"
+            icon="entypo:log-out"
+          />
+          <span className="hidden text-gray-600 lg:pl-2 lg:text-bold lg:flex">
+            Log Out
+          </span>
+        </div>
       </div>
     );
   };
@@ -105,12 +137,7 @@ function AdminSidebarComponent(props: any) {
           icon={"ant-design:user-add-outlined"}
           text={"Add User"}
         />
-        <AdminNavLink
-          toggle={"LogOut"}
-          link={"/logOut"}
-          icon={"entypo:log-out"}
-          text={"Log Out"}
-        />
+        <LogoutButton />
       </div>
       {props.children}
     </div>
