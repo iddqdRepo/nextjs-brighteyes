49e8b769cbffd5b7a521f71535c578c2ff22ffd4
iddqdRepo <chris.walker.beng@gmail.com>
2022-11-20 12:09:07 +0000

    Added wait to tests for middlware "<" bug

--- cypress/e2e/1-getting-started/addAnimalForm.cy.js
+++ cypress/e2e/1-getting-started/addAnimalForm.cy.js
@@ -36,33 +36,33 @@ describe("Add Animal Form", () => {
       "be.visible"
     );
 
-    // cy.get(".flex.flex-col.items-center.justify-end.mb-4.ml-1.mr-1").each(
-    //   ($el) => {
-    //     console.log("$el[0].children[1].type", $el[0].children[1].type);
-    //     if ($el[0].children[1].name === "age") {
-    //       let input = cy.wrap($el[0]).find("input");
-    //       input.type("55").blur();
-    //     } else if ($el[0].children[1].type === "text") {
-    //       let input = cy.wrap($el[0]).find("input");
-    //       input.type("hh").blur();
-    //     }
-    //     if ($el[0].children[1].type === "textarea") {
-    //       let input = cy.wrap($el[0]).find("textarea");
-    //       input.type("hh").blur();
-    //     }
+    cy.get(".flex.flex-col.items-center.justify-end.mb-4.ml-1.mr-1").each(
+      ($el) => {
+        console.log("$el[0].children[1].type", $el[0].children[1].type);
+        if ($el[0].children[1].name === "age") {
+          let input = cy.wrap($el[0]).find("input");
+          input.type("55").blur();
+        } else if ($el[0].children[1].type === "text") {
+          let input = cy.wrap($el[0]).find("input");
+          input.type("hh").blur();
+        }
+        if ($el[0].children[1].type === "textarea") {
+          let input = cy.wrap($el[0]).find("textarea");
+          input.type("hh").blur();
+        }
 
-    //     if ($el[0].children[1].type === "select-one") {
-    //       let input = cy.wrap($el[0]).find("select");
-    //       input.select(1).blur();
-    //     }
-    //   }
-    // );
+        if ($el[0].children[1].type === "select-one") {
+          let input = cy.wrap($el[0]).find("select");
+          input.select(1).blur();
+        }
+      }
+    );
 
-    // cy.fixture("CardImage.png", { encoding: null }).as("myFixture");
-    // cy.get("input[type=file]").selectFile("@myFixture");
+    cy.fixture("CardImage.png", { encoding: null }).as("myFixture");
+    cy.get("input[type=file]").selectFile("@myFixture");
 
-    // cy.get("button[type=submit]").click();
-    // cy.wait("@addPet");
-    // cy.contains("Submitted hh");
+    cy.get("button[type=submit]").click();
+    cy.wait("@addPet");
+    cy.contains("Submitted hh");
   });
 });
