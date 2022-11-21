3443294791d2a59113b1b7741b6457c213cf2be1
iddqdRepo <chris.walker.beng@gmail.com>
2022-11-21 10:04:34 +0000

    Changed server name for testing middleware

--- config/index.js
+++ config/index.js
@@ -2,4 +2,5 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://brighteyesanimalsanctuary.com";
+  : "https://nextjs-brighteyes-o2zv-iddqdrepo.vercel.app";
+// : "https://brighteyesanimalsanctuary.com";
