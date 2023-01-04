913e01b462aabaf03b28dcee5179d1b26e57b199
iddqdScript <chris.walker.beng@gmail.com>
2023-01-04 14:50:10 +0000

    Added head tag to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Map from "../components/LayoutComponents/HomeLayout/MapComponent/Map";
 import {
   FooterSection,
   ContactUsSection,
+  HeadTag,
 } from "../components/common/CommonComponents";
 import {
   HeroSection,
@@ -16,6 +17,13 @@ import NavbarComponent from "../components/Navbar/NavbarComponent";
 function Home() {
   return (
     <>
+      <HeadTag
+        title={"Home"}
+        metaContent={
+          "Bright Eyes was established in 1989 and is based in Co.Fermanagh, Northern Ireland."
+        }
+        linkHref={"/"}
+      />
       <NavbarComponent />
       <HeroSection />
       <AboutUsSection />
