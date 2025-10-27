cf408b3d51906a17cb975cd1f7be1b9b216c1c82
iddqd <chris.walker.beng@gmail.com>
2025-10-27 19:26:40 +0000

    Adding back in forms

--- components/FormPageLayout/FormLayoutComponents.tsx
+++ components/FormPageLayout/FormLayoutComponents.tsx
@@ -1,7 +1,6 @@
 import { Icon } from "@iconify/react";
 import { Button, ButtonWithQuery } from "../common/CommonComponents";
 import React from "react";
-import Link from "next/link";
 
 export const FormCard = ({
   title,
@@ -10,7 +9,6 @@ export const FormCard = ({
   buttonText,
   query,
   link,
-  hasButton = true,
 }: {
   title: string;
   text: string;
@@ -18,7 +16,6 @@ export const FormCard = ({
   buttonText: string;
   query?: string;
   link?: string;
-  hasButton?: boolean;
 }) => {
   const renderButton = () => {
     return query ? (
@@ -32,36 +29,36 @@ export const FormCard = ({
     );
   };
 
-  const temporaryContactUsNotAdoptionButton = () => {
-    return (
-      <div className="flex justify-center items-center max-w-fit p-1 ">
-        <span className="text-xs font-semibold text-white font-poppins text-[#8b3479] mt-5 mb-5">
-          We are short staffed at the moment, the forms are temporarily removed.
-          Please message us on Facebook.
-          <div className="flex items-center justify-center pb-2 mt-10">
-            <Link href={"https://www.facebook.com/brighteyes.a.s/"}>
-              <Icon
-                className="mr-2 cursor-pointer"
-                icon="akar-icons:facebook-fill"
-                color="#8b3479"
-                width="30"
-                height="30"
-              />
-            </Link>
-            {/* <a href={"https://www.instagram.com/brighteyesanimalsanctuary"}>
-              <Icon
-                className="mr-2 cursor-pointer"
-                icon="akar-icons:instagram-fill"
-                color="#8b3479"
-                width="30"
-                height="30"
-              />
-            </a> */}
-          </div>
-        </span>
-      </div>
-    );
-  };
+  // const temporaryContactUsNotAdoptionButton = () => {
+  //   return (
+  //     <div className="flex justify-center items-center max-w-fit p-1 ">
+  //       <span className="text-xs font-semibold text-white font-poppins text-[#8b3479] mt-5 mb-5">
+  //         We are short staffed at the moment, the forms are temporarily removed.
+  //         Please message us on Facebook.
+  //         <div className="flex items-center justify-center pb-2 mt-10">
+  //           <Link href={"https://www.facebook.com/brighteyes.a.s/"}>
+  //             <Icon
+  //               className="mr-2 cursor-pointer"
+  //               icon="akar-icons:facebook-fill"
+  //               color="#8b3479"
+  //               width="30"
+  //               height="30"
+  //             />
+  //           </Link>
+  //           {/* <a href={"https://www.instagram.com/brighteyesanimalsanctuary"}>
+  //             <Icon
+  //               className="mr-2 cursor-pointer"
+  //               icon="akar-icons:instagram-fill"
+  //               color="#8b3479"
+  //               width="30"
+  //               height="30"
+  //             />
+  //           </a> */}
+  //         </div>
+  //       </span>
+  //     </div>
+  //   );
+  // };
 
   return (
     <div className="flex flex-col w-80 h-100 bg-[url('../public/CardImage.png')] bg-no-repeat bg-center bg-cover mb-5 sm:mb-0 sm:mr-10 mt-10">
@@ -73,7 +70,7 @@ export const FormCard = ({
           {title}
         </span>
         {text}
-        {hasButton ? renderButton() : temporaryContactUsNotAdoptionButton()}
+        {renderButton()}
       </div>
     </div>
   );
